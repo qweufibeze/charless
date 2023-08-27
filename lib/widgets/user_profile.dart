38f92e7d5eb28645a956a3d3dbf0d5db17b25a4a
alexandrobol <qweufibeze@gmail.com>
2023-08-27 20:44:03 +0300

    день 2: немного подредактировал экран. Добавил стили для текста

--- lib/widgets/user_profile.dart
+++ lib/widgets/user_profile.dart
@@ -9,23 +9,90 @@ class UserProfile extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Example'),
       ),
-      body: SizedBox(
-        height: double.infinity,
+      body: const Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Container(
-                width: 100,
-                height: 100,
-                child: const Placeholder()
-            ),
-            const Text('alexander'),
-            const Text('+7 (981) 816 22 45'),
-            const Text('@alexandrobol')
+            _AvatarWidget(),
+            SizedBox(height: 30,),
+            _UserNameWidget(),
+            SizedBox(height: 10,),
+            _UserPhoneWidget(),
+            SizedBox(height: 10,),
+            _UserNickNameWidget()
           ],
         ),
       ),
+    );
+  }
+}
+
+class _UserNameWidget extends StatelessWidget {
+  const _UserNameWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'alexander',
+      style: TextStyle(
+          color: Colors.black,
+          fontSize: 25,
+          fontWeight: FontWeight.w600
+      ),
+    );
+  }
+}
+
+class _UserPhoneWidget extends StatelessWidget {
+  const _UserPhoneWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      '+7 (981) 816-22-45',
+      style: TextStyle(
+          color: Colors.grey,
+          fontSize: 15,
+          fontWeight: FontWeight.w600
+      ),
+    );
+  }
+}
+
+class _UserNickNameWidget extends StatelessWidget {
+  const _UserNickNameWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      '@alexandrobol',
+      style: TextStyle(
+          color: Colors.grey,
+          fontSize: 18,
+          fontWeight: FontWeight.w600
+      ),
+    );
+  }
+}
+
+class _AvatarWidget extends StatelessWidget {
+  const _AvatarWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+        width: 100,
+        height: 100,
+        child: Placeholder()
     );
   }
 }
